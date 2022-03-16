bba9fb5b173284aa51528c2b721e7d4fcd163169
claregippo <claregippo@localhost>
2022-03-16 10:26:07 +0100

    IC-2019.3.2 <claregippo@Clares-MacBook-Pro.local Create markdown.xml, studioFlags.xml, project.default.xml, Class.java, jdk.table.xml, Default.xml, laf.xml, filetypes.xml, vcs.xml, colors.scheme.xml, path.macros.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,15 @@
+// Program Name:
+// Name: Clare Gippo
+// VUnetID: gippoce
+// Email: clare.e.gippo@vanderbilt.edu
+// Class: CS 1101 - Vanderbilt University
+// Section: 001 MWF 11:10am
+// Date:
+// Honor statement: I attest that I understand the honor code for this class and have neither
+//                  given nor received unauthorized aid on this code.
+// Program description:
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
