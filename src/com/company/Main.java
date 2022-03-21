6a4e05916e0449acbe8c97d8eb0c6841fe5df794
Clare Gippo <claregippo@Clares-MacBook-Pro.local>
2022-03-21 09:50:35 +0100

    Service class setup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import com.company.Service;
 
 public class Main {
 
---
+++ src/com/company/Service.java
@@ -0,0 +1,8 @@
+// API Service
+// Contains basic function calls to and from our database.
+
+package com.company;
+
+public class Service {
+
+}
